8ee6833d71fb8cbd7b5d4f81d572586175066712
Mark <markjunyu@qq.com>
2022-04-30 17:32:30 +0800

    增加text和注释

--- radar_detect/reproject.py
+++ radar_detect/reproject.py
@@ -1,56 +1,72 @@
-'''
-反投影预警类
+"""
+反投影预警
 created by 牟俊宇 2020/12
 最新修改 by 牟俊宇 2021/1/15
-'''
+"""
+from typing import Union, Any, Tuple
+from numpy import ndarray
+
 import cv2
 import numpy as np
-import sys
-from resources.config import cam_config  # 相机参数
+import time
+
 from radar_detect.common import is_inside
-from resources.config import color2enemy, enemy_case, real_size, enemy_color, cam_config, \
-    DEBUG, test_region, region
+from resources.config import color2enemy, enemy_case, enemy_color, cam_config, \
+    DEBUG, test_region, region, real_size
 
 
 class Reproject(object):
     """
-    反投影预警
+    反投影预警类
     """
-    _iou_threshold = 0.8
+    _iou_threshold = 0.8  # iou阈值
+    _clock = 1  # 间隔一秒发送text
+    _frame = 3  # 一秒钟需要检测到3帧
 
-    def __init__(self, name):
+    def __init__(self, name, textapi):
+        # 读取相机配置文件
         self._tvec = cam_config[name]['tvec']
         self._rvec = cam_config[name]['rvec']
-        self.name = name
-        if DEBUG:
+        self._K_O = cam_config[name]['K_0']
+        self._C_O = cam_config[name]['C_0']
+        if DEBUG:   # 若为debug模式
             self._region = test_region
         else:
             self._region = region
-        self._scene_region = {}
-        self._color_bbox = None
-        self._size = (1024, 1024)
-        self._K_O = cam_config[name]['K_0']
-        self._C_O = cam_config[name]['C_0']
-        self._enemy = enemy_color
-        self._real_size = real_size
-        self._cache = None
-        self._debug = DEBUG
-        self._scene_init = False
-        self._pred_box = np.array([])
-        self.push_T(self._tvec, self._rvec)
+        self._scene_region = {}  # 反投影位置图形
+        # self._color_bbox = None
+        # self._size = (1024, 1024)
 
-    def _plot_regin(self):
+        self._enemy = enemy_color
+        self._real_size = real_size  # 真实尺寸
+        self._cache = None
+        # self._debug = DEBUG
+        self._scene_init = False  # 初始化flag
+        self._pred_box = np.array([])
+        self._textapi = textapi  # 发送text的api
+        self.rp_alarming = {}
+
+        self._region_count = {}  # 检测区域计数
+        self._time = {}  # 时间间隔
+        self._start = {}  # 开始时间
+        self._end = {}  # 结束时间
+
+        self._plot_regin()  # 预警区域坐标初始化
+
+    def _plot_regin(self) -> None:
         """
-        计算预警区域反投影坐标，用第一帧初始化了就可以了
-        return: 预警区域反投影坐标
+        计算预警区域反投影坐标，用第一帧初始化了就可以了。
         """
         for r in self._region.keys():
             # 格式解析
-            type, shape_type, team, location, height_type = r.split('_')
+            cor = None
+            height = None
+            rtype, shape_type, team, location, height_type = r.split('_')
             if location not in enemy_case or color2enemy[team] == self._enemy:  # 筛选出敌方的区域
-                if type == 's' or type == 'a':  # 筛选需要进行反投影的区域
-
-                    if shape_type == 'r':  # 绘制矩形
+                if rtype == 's' or rtype == 'a':  # 筛选需要进行反投影的区域
+                    self._region_count[f'{location}'] = 0  # 初始化区域检测计数
+                    self._time[f'{location}'] = 0  # 初始化时间间隔
+                    if shape_type == 'r':  # 计算矩形坐标
                         # 引入左上右下两点
                         lt = self._region[r][:2].copy()
                         rd = self._region[r][2:4].copy()
@@ -60,16 +76,13 @@ class Reproject(object):
                         # 另外两点坐标
                         ld = [lt[0], rd[1]]
                         rt = [rd[0], lt[1]]
-
                         cor = np.float32([lt, rt, rd, ld]).reshape(-1, 2)  # 坐标数组
-
                         if height_type == 'a':  # 四点在同一高度
                             height = np.ones((cor.shape[0], 1)) * self._region[r][4]
                         if height_type == 'd':  # 四点在不同高度
                             height = np.ones((cor.shape[0], 1))
                             height[1:3] *= self._region[r][5]  # 右上和右下
                             height[[0, 3]] *= self._region[r][4]  # 左上和左下
-
                     if shape_type == 'fp':
                         # 四点凸四边形类型，原理同上
                         cor = np.float32(self._region[r][:8]).reshape(-1, 2)
@@ -80,34 +93,32 @@ class Reproject(object):
                             height = np.ones((cor.shape[0], 1))
                             height[1:3] *= self._region[r][9]
                             height[[0, 3]] *= self._region[r][8]
+                    if isinstance(cor, np.ndarray) and isinstance(height, np.ndarray):
+                        cor = np.concatenate([cor, height], axis=1)  # 合并高度坐标
+                        recor = cv2.projectPoints(cor, self._rvec, self._tvec, self._K_O, self._C_O)[0] \
+                            .astype(int).reshape(-1, 2)  # 得到反投影坐标
+                        self._scene_region[r] = recor  # 储存反投影坐标
+                        self._scene_init = True
 
-                    cor = np.concatenate([cor, height], axis=1)  # 合并高度坐标
-
-                    recor = cv2.projectPoints(cor, self._rvec, self._tvec, self._K_O, self._C_O)[0].astype(int).reshape(
-                        -1, 2)  # 得到反投影坐标
-
-                    self._scene_region[r] = recor  # 储存反投影坐标
-        self._scene_init = True
-        return self._scene_region
-
-    def push_T(self, rvec, tvec):
+    def push_T(self, rvec: ndarray, tvec: ndarray) -> Tuple[Union[ndarray, Any], Any]:
         """
         输入相机位姿（世界到相机）
-        return:相机到世界变换矩阵（4*4）,相机世界坐标
+
+        Returns:
+            返回值: 相机到世界变换矩阵（4*4）,相机世界坐标
         """
-        self.rvec = rvec
-        self.tvec = tvec
-        # 初始化预警区域字典
-        self._plot_regin()
+        self._rvec = rvec
+        self._tvec = tvec
+        self._plot_regin()  # 初始化预警区域字典
         T = np.eye(4)
         T[:3, :3] = cv2.Rodrigues(rvec)[0]  # 旋转向量转化为旋转矩阵
         T[:3, 3] = tvec.reshape(-1)  # 加上平移向量
         T = np.linalg.inv(T)  # 矩阵求逆
         return T, (T @ (np.array([0, 0, 0, 1])))[:3]
 
-    def update(self, result, frame):
+    def update(self, result, frame) -> None:
         """
-        更新一帧
+        更新一帧绘图
         """
         for i in self._scene_region.keys():
             recor = self._scene_region[i]
@@ -127,14 +138,16 @@ class Reproject(object):
                 else:
                     cv2.polylines(frame, [recor], 1, (0, 255, 0))
 
-    def check(self, armors, cars):
-        '''
+    def check(self, armors: ndarray, cars: ndarray) -> None:
+        """
         预警预测
-        @param armors:N,cls+对应的车辆预测框序号+装甲板bbox
-        @param cars:N,cls+车辆bbox
-        '''
+
+        Args:
+            armors:N,cls+对应的车辆预测框序号+装甲板bbox
+            cars:N,cls+车辆bbox
+        """
         pred_bbox = np.array([])
-        rp_alarming = None
+
         color_bbox = []
         cache = None  # 当前帧缓存框
         id = np.array([1, 2, 3, 4, 5])
@@ -217,7 +230,7 @@ class Reproject(object):
                 mask = np.sum(mask, axis=1) > 0  # True or False,只要有一个点在区域内，则为True
                 alarm_target = cls[mask]  # 需要预警的装甲板种类
                 if len(alarm_target):
-                    rp_alarming = {r: alarm_target.reshape(1, -1)}
+                    self.rp_alarming = {r: alarm_target.reshape(1, -1)}
         # 储存为上一帧的框
         if isinstance(cache, np.ndarray):
             for i in id:
@@ -230,14 +243,35 @@ class Reproject(object):
     def get_pred_box(self):
         return self._pred_box
 
+    def push_text(self):
+        if self._scene_init:
+            str_output = ""
+            for r in self.rp_alarming.keys():
+                # 格式解析
+                _, _, _, location, _ = r.split('_')
+                if self._time[f'{location}'] == 0:
+                    self._start[f'{location}'] = time.time()
+                    self._region_count[f'{location}'] += 1
+                else:
+                    self._end[f'{location}'] = time.time()
+                    self._time[f'{location}'] = self._end[f'{location}'] - self._start[f'{location}']
+                    if self._time[f'{location}'] <= 1:
+                        self._region_count[f'{location}'] += 1
+                    if self._time[f'{location}'] == self._clock:
+                        self._time[f'{location}'] = 0
+                        if self._region_count[f'{location}'] == self._frame:
+                            str_output += f"在{location}处有敌人！！！\n"
+                            self._region_count[f'{location}'] = 0
+            self._textapi("WARNING", "反投影", str_output)
+        else:
+            self._textapi("WARNING", "反投影", "未初始化！！！")
+
 
 if __name__ == "__main__":
     ori = cv2.imread("/home/hoshino/CLionProjects/hitsz_radar/resources/beijing.png")
     frame = ori.copy()
     repo = Reproject(frame=frame, name='cam_left')
     _, _, region11 = repo.push_T(cam_config['cam_left']['rvec'], cam_config['cam_left']['tvec'])
-    from resources.config import cam_config, test_region, enemy_color, \
-        real_size
 
     frame = ori.copy()
     rect_armor = cv2.selectROI("img", frame, False)
